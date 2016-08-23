6184bf62e187bb4ec0a8be9231dfb72fa7c41947
Craig Webster <craig.webster@mac.com>
2016-08-23 21:15:43 +0100

    Initial Commit

---
+++ PCLaboratoryDataGeneratorSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PCLaboratoryDataGeneratorSwift
+//
+//  Created by Webster Craig on 23/08/2016.
+//  Copyright © 2016 Webster Craig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
