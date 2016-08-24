e69690099b56eb9c11b26efef067cd20f8493e41
Craig Webster <craig.webster@mac.com>
2016-08-24 21:45:55 +0100

    view controller setup

--- PCLaboratoryDataGeneratorSwift/ViewController.swift
+++ PCLaboratoryDataGeneratorSwift/ViewController.swift
@@ -10,9 +10,18 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var meanTextField: NSTextField!
+    @IBOutlet weak var sdTextField: NSTextField!
+    @IBOutlet weak var numResultsTextField: NSTextField!
+    var results = [Int:Double]()
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+    
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +29,13 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    
+    @IBAction func generateDistribution(sender: AnyObject) {
+        var res = PCGaussianData(mean: meanTextField.doubleValue, standardDeviation: sdTextField.doubleValue, numResultsRequired: numResultsTextField.integerValue)
+        
+        results = res.createDistribution()
     }
 
 
